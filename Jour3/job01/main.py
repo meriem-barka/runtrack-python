908e1d59bc23af2590da325cb85d71e7285e0fb5
Barka Meriem <mimiche@Air-de-Barka.lan>
2023-04-02 16:52:02 +0200

    ajout_job01

---
+++ Jour3/job01/main.py
@@ -0,0 +1,8 @@
+with open("domains.xml", "r") as text_file:
+    content = text_file.read()
+    print(content)
+
+with open("data.txt", "r") as text_file:
+    content = text_file.read()
+    print(content)
+
