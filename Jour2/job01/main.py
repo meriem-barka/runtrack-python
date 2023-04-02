50b5a6bcdbcd3132cf6460c94869144ab1723a19
Barka Meriem <mimiche@Air-de-Barka.lan>
2023-04-02 09:22:27 +0200

    ajout_job01

--- Jour2/job01/main.py
+++ Jour2/job01/main.py
@@ -1,32 +1,50 @@
-class Book:
-    def __init__(self, title, auteur):
-        self.title = title
+class Livre:
+    def __init__(self, titre, auteur):
+        self.titre = titre
         self.auteur = auteur
-       
-    def print(self):
-        print(self.title)
+
+    def getTitre(self):
+        print("Nom de l'auteur", self.auteur)
+
+livres1 = Livre("Harry Potter", "elisabethe")
+livres2 = Livre("Le seigneur des anneaux", "J.R.R. Tolkien")
+
+livres1.getTitre()
+
 
 
 class Personne:
-    def __init__(self, name, firstname):
-        self.name = name
-        self.firstname = firstname
+    def __init__(self, nom, prenom):
+        self.nom =  nom
+        self.prenom = prenom
 
 
-class Author(Personne):
-    def __init__(self, name, firstname):
-        super().__init__(name, firstname)
+
+class Auteur(Personne):
+    def __init__(self, nom, prenom):
+        super().__init__(nom, prenom)
         self.oeuvre = []
 
+    
 
     def listerOeuvre(self):
-        print(f"Liste des livres de {self.name} {self.firstname} :")
-        for livre in self.oeuvre:
-            print("- ", livre.title)
+        for livres in self.oeuvre:
+            print(livres.titre)
+
+    def ecrireUnLivre(self, titre):
+        livres = Livre(titre, self)
+        self.oeuvre.append(livres)       
+
+# créer un auteur
+auteur1 = Auteur("Dupont", "Pierre")
+
+# ajouter un nouveau livre à l'oeuvre de l'auteur
+nouveau_livre = auteur1.ecrireUnLivre("Le Voyage")
+nouveau_livre = auteur1.ecrireUnLivre("Les Vagues")
+
+# afficher la liste des livres de l'auteur pour vérifier que le nouveau livre a été ajouté
+auteur1.listerOeuvre()
 
 
-    def ecrireUnLivre(self, title):
-        books = Book(title, self)
-        self.oeuvre.append(books)
 
-        return books
+
